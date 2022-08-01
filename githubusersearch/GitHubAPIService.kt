a4566ed11cd4ea8a71a42ab3a7e244138d4cca54
eunjin2118 <ally6324@naver.com>
2022-08-01 11:44:26 +0900

    완료

--- githubusersearch/GitHubAPIService.kt
+++ githubusersearch/GitHubAPIService.kt
@@ -5,34 +5,40 @@ import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
 import retrofit2.http.Query
 import java.lang.reflect.Type
 
 interface GitHubAPIService {
-    @GET("/user/{id}")
-    fun getGitHubIdInfo(
-        @Path("id") id: Int
-    ) : Call<GitHubIdJSONResponseGSON>
+    @GET("/users/{id}")
+    fun getGitHubId(
+        @Path("id") id: String,
+        @Header("Authorization") pat:String
+    ) : Call<GitHubUser>
 
 }
 
-data class GitHubIdJSONResponseGSON(
+data class GitHubUser(
     val login: String,
-    val id: Int
+    val id: Int,
+    val name: String,
+    val followers: Int,
+    val following: Int,
+    val avatar_url: String
 )
-
-class GitHubResponseDeseralizerGSON : JsonDeserializer<GitHubIdJSONResponseGSON>{
-    override fun deserialize(
-        json: JsonElement?,
-        typeOfT: Type?,
-        context: JsonDeserializationContext?
-    ): GitHubIdJSONResponseGSON {
-        val root = json?.asJsonObject
-        val login = root?.getAsJsonPrimitive("login")?.getAsString()
-        val id = root?.getAsJsonPrimitive("id")?.getAsInt()
-
-        return GitHubIdJSONResponseGSON(login!!, id!!)
-    }
-
-}
+//
+//class GitHubResponseDeseralizerGSON : JsonDeserializer<GitHubUser>{
+//    override fun deserialize(
+//        json: JsonElement?,
+//        typeOfT: Type?,
+//        context: JsonDeserializationContext?
+//    ): GitHubUser {
+//        val root = json?.asJsonObject
+//        val login = root?.getAsJsonPrimitive("login")?.getAsString()
+//        val id = root?.getAsJsonPrimitive("id")?.getAsInt()
+//
+//        return GitHubUser(login!!, id!!)
+//    }
+//
+//}
--- githubusersearch/MainActivity.kt
+++ githubusersearch/MainActivity.kt
@@ -7,10 +7,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
-import android.widget.EditText
+import android.widget.*
 import androidx.viewpager.widget.ViewPager
+import com.bumptech.glide.Glide
 import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -20,22 +25,59 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val id = findViewById<EditText>(R.id.input_id)
+        val useridInput = findViewById<EditText>(R.id.input_id)
+        val button = findViewById<Button>(R.id.search)
+        val t_login = findViewById<TextView>(R.id.login_text)
+        val t_id = findViewById<TextView>(R.id.id_text)
+        val t_name = findViewById<TextView>(R.id.name)
+        val t_followers = findViewById<TextView>(R.id.followers)
+        val t_following = findViewById<TextView>(R.id.following)
+        val profile = findViewById<ImageView>(R.id.profile_image);
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.github.com")
             .addConverterFactory(
                 GsonConverterFactory.create(
-                    GsonBuilder().registerTypeAdapter(
-                        GitHubIdJSONResponseGSON::class.java,
-                        GitHubResponseDeseralizerGSON()
-                    ).create()
+//                    GsonBuilder().registerTypeAdapter(
+//                        GitHubUser::class.java,
+//                        GitHubResponseDeseralizerGSON()
+//                    ).create()
                 )
             )
             .build()
 
         val apiService = retrofit.create(GitHubAPIService::class.java)
-        val apiCallForData = apiService.getDustStatusInfo(login!!, id!!)
+        button.setOnClickListener {
+            val id = useridInput.text.toString()
+            val apiCallForData = apiService.getGitHubId(id, "token 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")
+            apiCallForData.enqueue(object : Callback<GitHubUser>{
+                override fun onResponse(call: Call<GitHubUser>, response: Response<GitHubUser>) {
+                    Log.d("mytag", response.code().toString())
+                    if((response.code().toString()).startsWith("4")) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "유저가 없습니다",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        val data = response.body()!!
+                        Log.d("mytag", data.toString())
+                        t_login.text = "login : ${data.login}"
+                        t_id.text = "id : ${data.id.toString()}"
+                        t_name.text = "name : ${data.name}"
+                        t_followers.text = "followers : ${data.followers.toString()}"
+                        t_following.text = "following : ${data.following.toString()}"
+                        Glide.with(this@MainActivity).load(data.avatar_url).into(profile)
+                    }
+
+                }
+
+                override fun onFailure(call: Call<GitHubUser>, t: Throwable) {
+
+                }
+
+            })
+        }
 
     }
 }
